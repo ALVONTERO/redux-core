581481857f00991b31cd0e66ec98a7f5e8deb3eb
ALVONTERO <83576414+ALVONTERO@users.noreply.github.com>
2023-04-11 04:01:10 +0200

    convert from redux to redux/toolkit
    Created reducer using createSlice(),
    also create store with configureStore()

--- src/store/index.js
+++ src/store/index.js
@@ -1,21 +1,43 @@
-import { createStore } from "redux";
+// import { createStore } from "redux";
+import { createSlice,configureStore } from "@reduxjs/toolkit";
 
 const initialState = { counter: 0, showCounter: true };
-const counterReducer = (state = initialState, action) => {
-  switch (action.type) {
-    case "increment":
-      return { counter: state.counter + 1, showCounter: state.showCounter };
-    case "increase":
-      return { counter: state.counter + action.amount };
-    case "decrement":
-      return { counter: state.counter - 1 };
-    case "toggle":
-      return { counter: state.counter, showCounter: !state.showCounter };
-    default:
-      return state;
-  }
-};
+const counterSlice= createSlice({
+  name: "counter",
+  initialState,
+  reducers: {
+    increment(state) {
+      state.counter++;
+    },
+    decrement(state) {
+      state.counter--;
+    },
+    increase(state, action) {
+      state.counter = state.counter + action.amount;
+    },
+    toggle(state) {
+      state.showCounter = !state.showCounter;
+    },
+  },
+});
 
-const store = createStore(counterReducer);
+const store = configureStore({reducer:counterSlice.reducer});
+
+
+// const counterReducer = (state = initialState, action) => {
+//   switch (action.type) {
+//     case "increment":
+//       return { counter: state.counter + 1, showCounter: state.showCounter };
+//     case "increase":
+//       return { counter: state.counter + action.amount };
+//     case "decrement":
+//       return { counter: state.counter - 1 };
+//     case "toggle":
+//       return { counter: state.counter, showCounter: !state.showCounter };
+//     default:
+//       return state;
+//   }
+// };
+
 
 export default store;
