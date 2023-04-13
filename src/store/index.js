3cf420b7fb0966d4c0a76d1c4e7495c2c5749358
ALVONTERO <83576414+ALVONTERO@users.noreply.github.com>
2023-04-13 06:37:51 +0200

    Finalizing Redux/Redux Toolkit

--- src/store/index.js
+++ src/store/index.js
@@ -1,48 +1,14 @@
 // import { createStore } from "redux";
-import { createSlice, configureStore } from "@reduxjs/toolkit";
+import { configureStore } from "@reduxjs/toolkit";
+import authSlice from "./auth";
+import counterSlice from "./counter";
 
-const initialCounterState = { counter: 0, showCounter: true };
-const initialAuthState = { auth: false };
-const counterSlice = createSlice({
-  name: "counter",
-  initialState: initialCounterState,
-  reducers: {
-    increment(state) {
-      state.counter++;
-    },
-    decrement(state) {
-      state.counter--;
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload;
-    },
-    toggleCounter(state) {
-      state.showCounter = !state.showCounter;
-    },
-  },
-});
-const AuthSlice = createSlice({
-  name: "authentication",
-  initialState: initialAuthState,
-  reducers: {
-    login(state) {
-      state.auth(true);
-    },
-
-    logout(state) {
-      state.auth(false);
-    },
-  },
-});
-
-export const counterActions = counterSlice.actions;
-export const authActions = AuthSlice.actions;
 
 
 const store = configureStore({
   reducer: { 
     counter: counterSlice.reducer, 
-    auth: AuthSlice.reducer 
+    auth: authSlice.reducer 
   },
 });
 
